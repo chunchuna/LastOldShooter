c0198ff6f7099bea959b2d0b493471994520e122
ccGameStudio <578806739@qq.com>
2020-11-22 02:59:07 +0800

    chunchun

--- godot_source/source/Game/Script/fCharacter/fCharacterAI/gCharacterWeapon.gd
+++ godot_source/source/Game/Script/fCharacter/fCharacterAI/gCharacterWeapon.gd
@@ -1,3 +1,15 @@
 extends Node2D
 
 # characterWeapon 
+
+# node 
+onready var sprite =$nCharacterWeapon/Sprite
+
+
+
+func _on_nCharacterArea2d_body_shape_entered(body_id, body, body_shape, area_shape):
+    # 范围检测
+    if body.name=="player":
+        bug.log("characterWeapon","玩家进入范围",false)
+        pass
+	
